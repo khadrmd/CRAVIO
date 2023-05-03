7c971cf4fdf0c620be60ae4dfd0c80f199c11678
Khaidir Mauladan <kdr.mauladan@gmail.com>
2023-05-04 01:28:25 +0700

    upd: backend, modules, templates, favicon

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const fs = require("fs");
+const favicon = require("express-favicon");
 
 dotenv.config({ path: "./config.env" });
+
+// MIDDLEWARE
+const replaceTemplate = require(`${__dirname}/modules/replaceTemplate`);
 
 // CONNECT TO DATABASE
 mongoose
@@ -23,14 +28,36 @@ mongoose
 
 // APP SETUP
 const app = express();
+const index = fs.readFileSync(`${__dirname}/public/HTML/index.html`, "utf-8");
+const raw = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+const data = JSON.parse(raw);
+const tempCard = fs.readFileSync(`${__dirname}/public/HTML/card.html`, "utf-8");
 
 // MODULES
 app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/HTML/index.html");
+  const cards = data.map((el) => replaceTemplate(tempCard, el)).join("");
+  const output = index.replace("{%RECIPE_CARDS%}", cards);
+  res.send(output);
 });
+
+// app.get("/recipes", (req, res) => {
+//   fs.readFile(`${__dirname}/data/data.json`, "utf-8", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500);
+//       return;
+//     }
+
+//     const recipes = JSON.parse(data);
+//     console.log(recipes);
+
+//     res.json(recipes);
+//   });
+// });
 
 // START SERVER
 const port = process.env.PORT;
