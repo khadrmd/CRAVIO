60617947b1bc622fbb5bc909808979227f3cfa68
Khaidir Mauladan <kdr.mauladan@gmail.com>
2023-05-04 17:09:37 +0700

    upd: layout, data, template, route

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -7,5 +7,26 @@ module.exports = (template, object) => {
   output = output.replace(/{%COST%}/g, object.cost);
   output = output.replace(/{%DESCRIPTION%}/g, object.description);
 
+  let list = "";
+  object.ingredients.forEach((el) => {
+    let li = "<li>" + el + "</li>";
+    list += li;
+  });
+  output = output.replace(/{%INGREDIENTS%}/g, list);
+
+  list = "";
+  object.cookwares.forEach((el) => {
+    let li = "<li>" + el + "</li>";
+    list += li;
+  });
+  output = output.replace(/{%COOKWARES%}/g, list);
+
+  list = "";
+  object.steps.forEach((el) => {
+    let li = "<li>" + el + "</li>";
+    list += li;
+  });
+  output = output.replace(/{%STEPS%}/g, list);
+
   return output;
 };
