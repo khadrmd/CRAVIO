34341cbcff436af58de70c801c322a21c409f74f
Khaidir Mauladan <kdr.mauladan@gmail.com>
2023-05-18 21:22:18 +0700

    upd: naming convention, page

--- scripts/tempScript.js
+++
@@ -1,9 +0,0 @@
-function test() {
-  const divs = document.getElementsByClassName("card");
-
-  for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener("click", (event) => {
-      divs[i].classList.toggle("enlarge");
-    });
-  }
-}
